701082884f062db9c9859ec518b28a92e5bbd80c
bendgk <benkosten@gmail.com>
2016-02-11 01:08:52 -0500

    Add some functionality...
    Added tick/1 upgrade to buy

--- Web Dev/Scripts/script.js
+++ Web Dev/Scripts/script.js
@@ -1,11 +1,37 @@
 var byte = 0;
+var clickAmount = 1;
+var costChipOne = 16;
+var amountChipOne = 0;
 
-function addByte() {
+function updateByte() {
     "use strict";
-    byte = byte + 1;
     if (byte === 1) {
         document.getElementById("bytes").innerHTML = byte.toString().concat(" Byte");
     } else {
         document.getElementById("bytes").innerHTML = byte.toString().concat(" Bytes");
+    }
+}
+
+function addByte() {
+    "use strict";
+    byte = byte + clickAmount;
+    updateByte();
+}
+
+function tick() {
+    "use strict";
+    updateByte();
+    byte = byte + amountChipOne;
+    document.getElementById("cost_one_hz_chip").innerHTML = "Cost: ".concat(costChipOne.toString());
+}
+
+var tickOnce = setInterval(tick, 1000);
+
+function buyChipOne() {
+    "use strict";
+    if (byte >= costChipOne) {
+        amountChipOne = amountChipOne + 1;
+        byte = byte - costChipOne;
+        costChipOne = costChipOne * 2;
     }
 }
