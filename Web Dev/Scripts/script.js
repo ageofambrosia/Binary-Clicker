74592c605d0b9ebd31f231531b47065d4534ece7
Helltek <phinobow@gmail.com>
2016-02-11 20:29:40 -0500

    Added function buyRam1kB(). Cleaned up code.
    Changed lots of names, cause I'm picky and for possible later
    convenience with similar names. Added function buyRam1kB(). Cleaned up
    some code, moved parts to different locations, added comments for self
    reminder. Some more nit picky stuff.

--- Web Dev/Scripts/script.js
+++ Web Dev/Scripts/script.js
@@ -1,39 +1,54 @@
 var byte = 0.0;
+var displayByte = byte.toFixed(0); //remove decimals (whats half a byte)
 var clickValue = 1;
-var amountChipOne = 0;
-var costChipOne = 16;
-var displayByte = byte.toFixed(0);
+
+var amountChip1Hz = 0;
+var costChip1Hz = 16;
+
+var amountRam1kB = 0;
+var costRam1kB = 4;
 
 function updateByte() {
     "use strict";
     if (byte === 1) {
-        document.getElementById("bytes").innerHTML = displayByte.toString().concat(" Byte");
+        document.getElementById("bytes").innerHTML = displayByte.toString().concat(" Byte"); //Displays 1 Byte
     } else {
-        document.getElementById("bytes").innerHTML = displayByte.toString().concat(" Bytes");
+        document.getElementById("bytes").innerHTML = displayByte.toString().concat(" Bytes"); // Displays n Bytes
     }
 }
 
 function addByte() {
     "use strict";
-    byte = byte + clickValue;
+    byte += clickValue;
     updateByte();
 }
 
 function tick() {
     "use strict";
-    byte = byte + amountChipOne / 10;
-    document.getElementById("cost_one_hz_chip").innerHTML = "Cost: ".concat(costChipOne.toString());
+    byte += amountChip1Hz / 10; //divide 10 due to interval being 10 times faster
     updateByte();
-    displayByte = byte.toFixed(0);
+    displayByte = byte.toFixed(0); //remove decimals (whats half a byte)
 }
 
-var tickOnce = setInterval(tick, 100);
+var tickOnce = setInterval(tick, 100); //sets tick() interval
 
-function buyChipOne() {
+function buyChip1Hz() {
     "use strict";
-    if (byte >= costChipOne) {
-        amountChipOne = amountChipOne + 1;
-        byte = byte - costChipOne;
-        costChipOne = costChipOne * 2;
+    if (byte >= costChip1Hz) {
+        amountChip1Hz += 1;
+        byte -= costChip1Hz;
+        costChip1Hz *= 2;
+        document.getElementById("cost_one_hz_chip").innerHTML = "Cost: ".concat(costChip1Hz.toString()); //change displayed cost
+    }
+}
+
+function buyRam1kB() {
+    "use strict";
+    if (byte >= costRam1kB) {
+        amountRam1kB += 1;
+        clickValue += 1;
+        byte -= costRam1kB;
+        costRam1kB *= 2;
+        document.getElementById("cost_one_kb_ram").innerHTML = "Cost: ".concat(costRam1kB.toString()); //change displayed cost
     }
 }
