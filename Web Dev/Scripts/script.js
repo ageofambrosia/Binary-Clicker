4f9b6967d7e6b08ddf83030c3de4a43a9495f97a
bendgk <benkosten@gmail.com>
2016-02-11 19:37:40 -0500

    Added smoother TIcking
    SCIENCE! AND MATH with a slight bit of ENGINEERING

--- Web Dev/Scripts/script.js
+++ Web Dev/Scripts/script.js
@@ -1,31 +1,33 @@
-var byte = 0;
-var clickAmount = 1;
-var costChipOne = 16;
+var byte = 0.0;
+var clickValue = 1;
 var amountChipOne = 0;
+var costChipOne = 16;
+var displayByte = byte.toFixed(0);
 
 function updateByte() {
     "use strict";
     if (byte === 1) {
-        document.getElementById("bytes").innerHTML = byte.toString().concat(" Byte");
+        document.getElementById("bytes").innerHTML = displayByte.toString().concat(" Byte");
     } else {
-        document.getElementById("bytes").innerHTML = byte.toString().concat(" Bytes");
+        document.getElementById("bytes").innerHTML = displayByte.toString().concat(" Bytes");
     }
 }
 
 function addByte() {
     "use strict";
-    byte = byte + clickAmount;
+    byte = byte + clickValue;
     updateByte();
 }
 
 function tick() {
     "use strict";
-    updateByte();
-    byte = byte + amountChipOne;
+    byte = byte + amountChipOne / 10;
     document.getElementById("cost_one_hz_chip").innerHTML = "Cost: ".concat(costChipOne.toString());
+    updateByte();
+    displayByte = byte.toFixed(0);
 }
 
-var tickOnce = setInterval(tick, 1000);
+var tickOnce = setInterval(tick, 100);
 
 function buyChipOne() {
     "use strict";
