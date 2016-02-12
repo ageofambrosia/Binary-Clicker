852581776ae9e41ac61a92a0fe78fa9e255c0583
Helltek <phinobow@gmail.com>
2016-02-12 15:04:27 -0500

    Added arguments for functions
    As stated. Also more nitpicky stuff.

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,10 +2,10 @@ var byte = 0.0;
 var displayByte = byte.toFixed(0); //remove decimals (whats half a byte)
 var clickValue = 1;
 
-var amountChip1Hz = 0;
+var amtChip1Hz = 0;
 var costChip1Hz = 16;
 
-var amountRam1kB = 0;
+var amtRam1kB = 0;
 var costRam1kB = 4;
 
 function updateByte() {
@@ -25,30 +25,30 @@ function addByte() {
 
 function tick() {
     "use strict";
-    byte += amountChip1Hz / 10; //divide 10 due to interval being 10 times faster
+    byte += amtChip1Hz / 10; //divide 10 due to interval being 10 times faster
     updateByte();
     displayByte = byte.toFixed(0); //remove decimals (whats half a byte)
 }
 
 var tickOnce = setInterval(tick, 100); //sets tick() interval
 
-function buyChip1Hz() {
+function buyChip1Hz(amt) {
     "use strict";
-    if (byte >= costChip1Hz) {
-        amountChip1Hz += 1;
-        byte -= costChip1Hz;
-        costChip1Hz *= 2;
-        document.getElementById("cost_one_hz_chip").innerHTML = "Cost: ".concat(costChip1Hz.toString()); //change displayed cost
+    if (byte >= (costChip1Hz * (Math.pow(2, amt) - 1))) {
+        byte -= (costChip1Hz * (Math.pow(2, amt) - 1));
+        amtChip1Hz += amt;
+        costChip1Hz *= Math.pow(2, amt);
+        document.getElementById("cost_one_hz_chip").innerHTML = "Cost: ".concat(costChip1Hz).toString(); //change displayed cost
     }
 }
 
-function buyRam1kB() {
+function buyRam1kB(amt) {
     "use strict";
-    if (byte >= costRam1kB) {
-        amountRam1kB += 1;
-        clickValue += 1;
-        byte -= costRam1kB;
-        costRam1kB *= 2;
-        document.getElementById("cost_one_kb_ram").innerHTML = "Cost: ".concat(costRam1kB.toString()); //change displayed cost
+    if (byte >= (costRam1kB * (Math.pow(2, amt) - 1))) {
+        byte -= (costRam1kB * (Math.pow(2, amt) - 1));
+        amtRam1kB += amt;
+        clickValue += amt;
+        costRam1kB *= Math.pow(2, amt);
+        document.getElementById("cost_one_kb_ram").innerHTML = "Cost: ".concat(costRam1kB).toString(); //change displayed cost
     }
 }
